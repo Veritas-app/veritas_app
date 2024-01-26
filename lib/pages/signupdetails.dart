99a16e02d82bc3674f7af411926bb93eb1e50607
shaaradnj <shaaradjarandikar@gmail.com>
2024-01-26 14:19:20 +0530

    signupdetails changes

---
+++ lib/pages/signupdetails.dart
@@ -0,0 +1,21 @@
+// import 'package:flutter/material.dart';
+
+// class signInDetails extends StatelessWidget {
+//   const signInDetails({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.blueAccent[100],
+//         body: Center(
+//           child: Text(
+//             "Name",
+//             style: TextStyle(),
+//           ),
+//         )
+       
+//       )
+//     );
+//   }
+// }   
