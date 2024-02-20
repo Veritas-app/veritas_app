41ee26ffeb333a04e827364b3d046da16c12e580
Anuvind-ap <anuvindap2016@gmail.com>
2024-02-20 15:30:06 +0530

    removed unused imports

--- lib/pages/signupdetails_lawyer.dart
+++ lib/pages/signupdetails_lawyer.dart
@@ -1,8 +1,6 @@
-// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:veritas/pages/client/clientdashboard.dart';
 import 'package:veritas/pages/lawyer/lawyerdashboard.dart';
 
 
